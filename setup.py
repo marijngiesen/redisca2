0b558f7485700967a3611da341e566af9d97e46c
marijngiesen <marijn@studio-donder.nl>
2016-11-07 20:57:17 +0100

    Fixed formatting

--- setup.py
+++ setup.py
@@ -2,43 +2,41 @@
 # -*- coding: utf-8 -
 
 from setuptools import (
-	setup,
-	find_packages,
+    setup,
+    find_packages,
 )
-
 
 PACKAGES = find_packages()
 
-
 setup(
-	name = 'redisca2',
-	version = '1a1',
-	packages = PACKAGES,
-	package_dir = {'': '.'},
-	test_suite = 'redisca2.tests',
-	author = 'Vitaliy Khamin',
-	author_email = 'vitaliykhamin@gmail.com',
-	maintainer = 'Vitaliy Khamin',
-	maintainer_email = 'vitaliykhamin@gmail.com',
-	description = 'Lightweight ORM for Redis',
-	url = 'http://github.com/khamin/redisca2',
-	zip_safe = True,
+    name='redisca2',
+    version='1a1',
+    packages=PACKAGES,
+    package_dir={'': '.'},
+    test_suite='redisca2.tests',
+    author='Vitaliy Khamin',
+    author_email='vitaliykhamin@gmail.com',
+    maintainer='Vitaliy Khamin',
+    maintainer_email='vitaliykhamin@gmail.com',
+    description='Lightweight ORM for Redis',
+    url='http://github.com/khamin/redisca2',
+    zip_safe=True,
 
-	platforms = (
-		'any'
-	),
+    platforms=(
+        'any'
+    ),
 
-	classifiers = (
-		'Operating System :: OS Independent',
-		'Development Status :: 4 - Beta',
-		'Programming Language :: Python :: 2.7',
-		'Programming Language :: Python :: 3.2',
-		'Programming Language :: Python :: 3.3',
-		'Programming Language :: Python :: 3.4',
-		'Topic :: Database'
-	),
+    classifiers=(
+        'Operating System :: OS Independent',
+        'Development Status :: 4 - Beta',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Database'
+    ),
 
-	install_requires = [
-		'redis >= 2.7'
-	]
+    install_requires=[
+        'redis >= 2.7'
+    ]
 )
